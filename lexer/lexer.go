a6996f4e58eb15abadb43d1cfc660106d3bbca68
Alex Shi <shi13341455279@gmail.com>
2024-08-12 19:13:13 +0800

    feat: Implement part Lexer functions

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,1 +1,27 @@
 package lexer
+
+// Lexer 词法分析器
+type Lexer struct {
+	input   string
+	pos     int
+	readPos int // 始终指向当前字符的下一个字符
+	ch      byte
+}
+
+// New 初始化词法分析器
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// readChar 读取下一个字符
+func (l *Lexer) readChar() {
+	if l.readPos >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPos]
+	}
+	l.pos = l.readPos
+	l.readPos++
+}
